29c464bfe01f73cd70ee7465e311eb81c25cf19c
cilame <opaquism@hotmail.com>
2021-09-26 23:34:49 +0800

    add

--- background.js
+++ background.js
@@ -1,2 +1,1 @@
 // background.js
-
--- popup.js
+++ popup.js
@@ -1,10 +1,22 @@
+// var background = chrome.extension.getBackgroundPage();
+// var gettabid = background.gettabid
+
+function sendMessageToContentScript(message, callback){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, message, function(response){
+      if(callback) callback(response);
+    });
+  });
+}
+
 document.querySelectorAll("input").forEach(function(v){
-    chrome.storage.sync.get([v.dataset.key], function (result) {
-        v.checked = result[v.dataset.key];
+  chrome.storage.sync.get([v.dataset.key], function (result) {
+    v.checked = result[v.dataset.key];
+  })
+  v.addEventListener("change", function (e) {
+    sendMessageToContentScript({[e.target.dataset.key]: e.target.checked})
+    chrome.storage.sync.set({
+      [e.target.dataset.key]: e.target.checked
     })
-    v.addEventListener("change", function (e) {
-        chrome.storage.sync.set({
-            [e.target.dataset.key]: e.target.checked
-        })
-    })
+  })
 })
