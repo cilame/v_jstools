d3125bede3bb87012518d8ef798b64d7b5324ac5
cilame <opaquism@hotmail.com>
2023-02-11 00:40:34 +0800

    add

---
+++ devtools.js
@@ -0,0 +1,17 @@
+chrome.devtools.panels.create(
+  'v_opitons',
+  null,
+  'options.html'
+);
+
+chrome.devtools.panels.create(
+  'v_diff',
+  null,
+  'diff_text.html'
+);
+
+chrome.devtools.panels.create(
+  'v_ast',
+  null,
+  'astexplorer_babel.html'
+);
